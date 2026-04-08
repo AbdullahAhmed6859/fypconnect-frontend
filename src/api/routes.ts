a8f97d20f9ddd1f8b6d9b4504877e488a2cd0344
Amn Naqvi <sn08776@st.habib.edu.pk>
2026-04-08 14:56:24 +0500

    Rayyan changes

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:5000/api/v1";
+export const API_BASE_URL = "http://localhost:3000/api/v1";
 
 export const authRoutes = {
   login: `${API_BASE_URL}/auth/login`,
